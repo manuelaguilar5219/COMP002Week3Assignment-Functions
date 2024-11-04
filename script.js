561e8e930163959531265f65785b052723274b9f
manuelaguilar5219 <manuelaguilar5219@my.whccd.edu>
2024-11-03 19:07:46 -0800

    fixed question 1 and 3

--- script.js
+++ script.js
@@ -2,14 +2,28 @@
 // Examples:
 // console.log(min(0, 10)); // will output 0.
 // console.log(min(0, -10)); // will output -10
-console.log(min(5, 20)); // Will Output 5
+console.log(Math.min(5, 20)); // Will Output 5
 // 2. Write a function that takes the radius of a circle and outputs it’s approximate circumference (the formula for circumference is 2 *pi * radius, you may use 3.14 for pi).
 // Example Output:
 // console.log(calculateCircumference(5)); // will output 31.4
+function calculateCircumference(radius) {
+    const pi = 3.14
+    return 2 * pi * radius;
+}
+
 console.log(calculateCircumference(10));
 // Will Output: 62.8
 // 3. Write a function searchString that takes a character and then returns how many times it appears in another string argument.
 // Example Output:
 // console.log(searchString(“s”, “mississippi”)); // will output 4
 // console.log(searchString(“q”, “mississippi”)); // will output 0
+function searchString(char, str) {
+    let count = 0;
+    for (let i = 0; i < str.length; i++) {
+        if (str[i] === char) {
+            count++;
+        }
+    }
+    return count;
+}
 console.log(searchString("m", "mississippi")); // Will Output 1 because m appears once in the string
